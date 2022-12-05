fddb06d0ce8fbeac31214febfbc318a1dc011cd5
Michael Harrop <michael.harrop94@gmail.com>
2022-12-05 15:58:30 -0700

    modified to start testing functionality

--- PDFer/Program.cs
+++ PDFer/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using iText;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using PDFer;
+
+ReportCard rc = new ReportCard("/Users/michaelharrop/Projects/Work/8 - North Cache.pdf.pdf",
+    "/Users/michaelharrop/Projects/Work/Split");
+
+PdfDocument pdfDoc = new PdfDocument(new PdfReader(rc.Source), new PdfWriter(rc.Destination));
+Document document = new Document(pdfDoc);
+
+pdfDoc.AddPage(rc.GetPage(1));
