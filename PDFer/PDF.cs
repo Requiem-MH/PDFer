2c8bb658e631f4259d0fd5d66bfe7436c59579c2
Michael Harrop <michael.harrop94@gmail.com>
2022-12-05 12:27:52 -0700

    Created abstract PDF class to derive from

---
+++ PDFer/PDF.cs
@@ -0,0 +1,39 @@
+using IronPdf;
+
+namespace PDFer;
+abstract class PDF
+{
+    protected string Path { get; }
+    protected string Source { get; }
+    protected string Destination { get; }
+    protected PdfDocument Pdf { get; }
+    protected int Pages { get; }
+
+    public PDF(string source, string destination)
+    {
+        this.Path = System.IO.Path.GetDirectoryName(source);
+        this.Source = source;
+        this.Destination = destination;
+        this.Pdf = PdfDocument.FromFile(source);
+        this.Pages = Pdf.PageCount;
+    }
+
+    public void Save(string fileName)
+    {
+        try
+        {
+            if (!Directory.Exists(Destination))
+            {
+                Directory.CreateDirectory(Destination);
+            }
+
+            this.Pdf.SaveAs(Destination + "/" + fileName);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The process failed; {0}", e.ToString());
+        }
+    }
+    
+    public abstract void Extract();
+}
